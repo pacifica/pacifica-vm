e9984dab1ab17a2b858247cc20f0d2c3e97cbd25
David Brown <dmlb2000@gmail.com>
2020-04-24 11:46:49 -0700

    Add Verification of the Docker Container (#7)
    This changes the workflow of the pipeline a bit to verify the built
    container instead of the running configured container.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/buildit.sh
+++ scripts/buildit.sh
@@ -2,5 +2,6 @@
   
 docker build -t travis_local/pacifica/pacifica-vm:latest .
 docker run -d --name=pacifica-vm -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged travis_local/pacifica/pacifica-vm:latest
-docker exec pacifica-vm ansible-playbook /etc/ansible/pacifica-vm/playbook.yml
+docker exec pacifica-vm ansible-playbook /etc/ansible/pacifica-vm/setup-play.yml
 docker exec pacifica-vm ansible-playbook /etc/ansible/drupal-vm-5.2.0/provisioning/playbook.yml
+docker exec pacifica-vm ansible-playbook /etc/ansible/pacifica-vm/playbook.yml
--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -xe
 
+docker run -d --name=pacifica-vm -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged pacifica/pacifica-vm:latest
+sleep 60
 docker exec pacifica-vm ansible-playbook /etc/ansible/pacifica-vm/verify.yml
