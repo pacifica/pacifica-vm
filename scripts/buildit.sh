cec2aea6c57b597e28f5bfda08fd06c0fae94f42
David Brown <dmlb2000@gmail.com>
2020-08-10 09:02:26 -0700

    v0.6.0: Update Drupal VM
    This updates Drupal VM to 6.0.0 and associated defaults.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- scripts/buildit.sh
+++ scripts/buildit.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(dirname $0)
 source "${SCRIPT_DIR}/vars.sh"
 
 docker build -t travis_local/pacifica/pacifica-vm:latest .
-docker run -d --name=pacifica-vm -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged travis_local/pacifica/pacifica-vm:latest
+docker run -d --hostname=pacifica-vm --name=pacifica-vm -v /sys/fs/cgroup:/sys/fs/cgroup:ro --privileged travis_local/pacifica/pacifica-vm:latest
 docker exec pacifica-vm ansible-playbook -e keep_vcs=${KEEP_VCS} /etc/ansible/pacifica-vm/setup-play.yml
-docker exec pacifica-vm ansible-playbook /etc/ansible/drupal-vm-5.2.0/provisioning/playbook.yml
+docker exec pacifica-vm ansible-playbook /etc/ansible/drupal-vm-6.0.0/provisioning/playbook.yml
 docker exec pacifica-vm ansible-playbook -e keep_vcs=${KEEP_VCS} /etc/ansible/pacifica-vm/playbook.yml
